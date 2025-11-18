535197b9c37ce271227981b27344181f659f8e88
Pedro Manoel <ps6711534@gmail.com>
2025-11-18 10:46:35 -0300

    fix/mudei-jtablealunos-para-jtableprodutos

--- src/Controller/ProdutoControllers.java
+++ src/Controller/ProdutoControllers.java
@@ -107,15 +107,15 @@ public class ProdutoControllers {
     }
     
     
-    public void ApagarProduto(JTable jTableAlunos, JTextField c_nome_produto, JTextField c_descricao, JTextField c_quantidade,JTextField c_preco){
+    public void ApagarProduto(JTable jTableProdutos, JTextField c_nome_produto, JTextField c_descricao, JTextField c_quantidade,JTextField c_preco){
         
         try {
             // validando dados da interface gr�fica.
             int id = 0;
-            if (jTableAlunos.getSelectedRow() == -1) {
+            if (jTableProdutos.getSelectedRow() == -1) {
                 throw new Mensagens("Primeiro Selecione um Produto para APAGAR");
             } else {
-                id = Integer.parseInt(jTableAlunos.getValueAt(jTableAlunos.getSelectedRow(), 0).toString());
+                id = Integer.parseInt(jTableProdutos.getValueAt(jTableProdutos.getSelectedRow(), 0).toString());
             }
 
             // retorna 0 -> primeiro bot�o | 1 -> segundo bot�o | 2 -> terceiro bot�o
@@ -143,13 +143,13 @@ public class ProdutoControllers {
             JOptionPane.showMessageDialog(null, erro.getMessage());
         } finally {
             // atualiza a tabela.
-            this.carregaTabela(jTableAlunos);
+            this.carregaTabela(jTableProdutos);
         }
 
 
     }
     
-    public void alterarProduto(JTable jTableAlunos, JTextField c_nome_produto, JTextField c_descricao, JTextField c_quantidade,JTextField c_preco){
+    public void alterarProduto(JTable jTableProdutos, JTextField c_nome_produto, JTextField c_descricao, JTextField c_quantidade,JTextField c_preco){
         try {
             // recebendo e validando dados da interface gr�fica.
             String nome = "";
@@ -183,10 +183,10 @@ public class ProdutoControllers {
                 preco = Double.parseDouble(c_preco.getText());
             }
 
-            if (jTableAlunos.getSelectedRow() == -1) {
+            if (jTableProdutos.getSelectedRow() == -1) {
                 throw new Mensagens("Primeiro Selecione um Aluno para Alterar");
             } else {
-                id = Integer.parseInt(jTableAlunos.getValueAt(jTableAlunos.getSelectedRow(), 0).toString());
+                id = Integer.parseInt(jTableProdutos.getValueAt(jTableProdutos.getSelectedRow(), 0).toString());
             }
 
             Produto objProduto = new Produto(id,nome,descricao,qntEstoque,preco);
@@ -207,7 +207,7 @@ public class ProdutoControllers {
         } catch (NumberFormatException erro2) {
             JOptionPane.showMessageDialog(null, "Informe um número.");
         } finally {
-            this.carregaTabela(jTableAlunos);
+            this.carregaTabela(jTableProdutos);
         }
     }
 }
